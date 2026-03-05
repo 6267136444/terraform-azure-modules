c48fe8c9da0ad686ffd75ba5ec2f85b0f6d0df07
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 12:30:36 +0530

    Update main.tf

--- modules/linux-vm/main.tf
+++ modules/linux-vm/main.tf
@@ -1,64 +1,113 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "CCRH-RG"
+  location = "Central India"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm_name}-vnet"
+  name                = "CCRH-Vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.vm_name}-subnet"
+resource "azurerm_subnet" "web_subnet" {
+  name                 = "web-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "${var.vm_name}-pip"
-  location            = var.location
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "web_pip" {
+  name                = "webserver-pip"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
+resource "azurerm_network_interface" "web_nic" {
+  name                = "web-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
+    subnet_id                     = azurerm_subnet.web_subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.4"
+    public_ip_address_id          = azurerm_public_ip.web_pip.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  location            = var.location
+resource "azurerm_network_interface" "db_nic" {
+  name                = "db-nic"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  disable_password_authentication = false
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.db_subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.2.4"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "web_vm" {
+  name                = "server-webserver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2as_v5"
+  admin_username      = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.nic.id
+    azurerm_network_interface.web_nic.id
   ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 100
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts"
+  version   = "latest"
+}
+
+  disable_password_authentication = false
+}
+
+resource "azurerm_linux_virtual_machine" "db_vm" {
+  name                = "server-dbserver"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D4als_v6"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.db_nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 100
   }
+
+  source_image_reference {
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts"
+  version   = "latest"
+}
+
+  disable_password_authentication = false
 }
