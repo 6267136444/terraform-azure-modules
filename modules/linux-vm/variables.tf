d4122a3e1d67fba902eb0634628161257b040301
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 12:47:12 +0530

    Update variables.tf

--- modules/linux-vm/variables.tf
+++ modules/linux-vm/variables.tf
@@ -1,9 +1,5 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "vm_name" { type = string }
-variable "vm_size" { type = string }
-variable "admin_username" { type = string }
 variable "admin_password" {
-  type      = string
-  sensitive = true
+  description = "VM Admin Password"
+  type        = string
+  sensitive   = true
 }
