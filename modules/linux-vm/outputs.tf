ec7564539d752fc4484fd817be231b4f54b7ac89
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2026-03-05 12:43:39 +0530

    Update outputs.tf

--- modules/linux-vm/outputs.tf
+++ modules/linux-vm/outputs.tf
@@ -1,7 +1,11 @@
-output "vm_name" {
-  value = azurerm_linux_virtual_machine.vm.name
+output "web_vm_name" {
+  value = azurerm_linux_virtual_machine.web_vm.name
 }
 
-output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+output "db_vm_name" {
+  value = azurerm_linux_virtual_machine.db_vm.name
+}
+
+output "web_public_ip" {
+  value = azurerm_public_ip.web_pip.ip_address
 }
